82b276ea72df76b13d34caef55c231432159c518
TomKALBE <91056619+TomKALBE@users.noreply.github.com>
2022-02-04 17:15:04 +0100

    Update exercice1.ts

--- src/exercice1.ts
+++ src/exercice1.ts
@@ -1,39 +1,23 @@
-import { string } from "prop-types";
-
-export type User = userInterface;
-export const users: userInterface[] = [
+export type User = any;
+export const users: any[] = [
  {
-    name: 'Alain',
-    age: 25,
-    occupation: 'Boulanger',
-    compentences : ["js", "node"]
-
+ name: 'Alain',
+ age: 25,
+ occupation: 'Boulanger',
+ compentences : ["js", "node"]
+ 
  },
  {
-    name: 'Béatrice',
-    age: 23,
-    occupation: 'Astronaute',
-    adresse : {
-    rue : "75 rue de Paris",
-    cp : 75000,
-    ville : "Paris"
+ name: 'Béatrice',
+ age: 23,
+ occupation: 'Astronaute',
+ adresse : {
+ rue : "75 rue de Paris",
+ cp : 75000,
+ ville : "Paris"
 }
  }
 ];
-interface userInterface{
-    name:string,
-    age:number,
-    occupation:string,
-    compentences?:Array<string>,
-    adresse?:adresseInterface,
-
-}
-
-interface adresseInterface{
-    rue:string,
-    cp:number,
-    ville:string
-}
-export function logPerson(user: userInterface) {
+export function logPerson(user: any) {
  console.log(` - ${user.name}, ${user.age}`);
 }
