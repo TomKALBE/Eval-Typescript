3c7d7ff8d5bfe5a1ec2729e919db12ec9807c488
TomKALBE <91056619+TomKALBE@users.noreply.github.com>
2022-02-04 17:40:12 +0100

    Update exercice1.ts

--- src/exercice1.ts
+++ src/exercice1.ts
@@ -1,23 +1,30 @@
-export type User = any;
-export const users: any[] = [
- {
- name: 'Alain',
- age: 25,
- occupation: 'Boulanger',
- compentences : ["js", "node"]
- 
- },
- {
- name: 'Béatrice',
- age: 23,
- occupation: 'Astronaute',
- adresse : {
- rue : "75 rue de Paris",
- cp : 75000,
- ville : "Paris"
-}
- }
+export type User = {
+    name: string,
+    age: number,
+    occupation: string,
+    compentences?: Array<string>,
+    adresse?: any
+};
+
+export const users: User[] = [
+    {
+        name: 'Alain',
+        age: 25,
+        occupation: 'Boulanger',
+        compentences: ["js", "node"]
+    },
+    {
+        name: 'Béatrice',
+        age: 23,
+        occupation: 'Astronaute',
+        adresse: {
+            rue: "75 rue de Paris",
+            cp: 75000,
+            ville: "Paris"
+        }
+    }
 ];
-export function logPerson(user: any) {
- console.log(` - ${user.name}, ${user.age}`);
+
+export function logPerson(user: User) {
+    console.log( - ${user.name}, ${user.age});
 }
